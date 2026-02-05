8d38c689b05df63fc6529703cd6612c391e117a7
jamesshenry <git@mail.jamesshenry.dev>
2026-02-05 16:27:42 +0000

    add velopack

--- Fusion++/Program.cs
+++ Fusion++/Program.cs
@@ -2,23 +2,13 @@
 using FusionPlusPlus.Forms;
 using System;
 using System.Windows.Forms;
+using Velopack;
 
-namespace FusionPlusPlus
-{
-	static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-			UserLookAndFeel.Default.SetSkinStyle(SkinSvgPalette.Bezier.OfficeBlack);
+VelopackApp.Build().Run();
 
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm());
-		}
-	}
-}
+UserLookAndFeel.Default.SetSkinStyle(SkinSvgPalette.Bezier.OfficeBlack);
+
+Application.SetHighDpiMode(HighDpiMode.SystemAware);
+Application.EnableVisualStyles();
+Application.SetCompatibleTextRenderingDefault(false);
+Application.Run(new MainForm());
